92d6105a70b104aff791f9ea477b631387731294
guilherme <49197151+Guilherme-Vasconcelos@users.noreply.github.com>
2021-05-31 05:14:22 -0300

    Add horizontal scrolling and memmove

--- lib/string.c
+++ lib/string.c
@@ -2,11 +2,6 @@
 
 #include "string.h"
 
-/**
- * calculates the length of a string.
- * @param string a valid null-terminated string.
- * @return the length of the string.
- */
 size_t strlen(const char *string)
 {
     size_t len = 0;
@@ -16,4 +11,29 @@ size_t strlen(const char *string)
     }
 
     return len;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+    // FIXME: could these casts possibly cause issues in the future?
+    size_t *tmp_dest = (size_t *)dest;
+    size_t *tmp_src = (size_t *)src;
+
+    // If dest and src are overlapping, directly overwriting dest could
+    // make src possibly lose important data that it shouldn't lose yet
+    // hence why use the tmp buffer.
+    size_t tmp[n];
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        tmp[i] = tmp_src[i];
+    }
+
+    for (size_t i = 0; i < n; ++i)
+    {
+        tmp_dest[i] = tmp[i];
+    }
+
+    dest = tmp_dest;
+    return (void *)dest;
 }
